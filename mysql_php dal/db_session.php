ba13b0212c5fbd153d75d982d02238ebdaf2c289
koomasha <koomasha@gmail.com>
2015-02-04 01:04:51 +0200

    hide

--- mysql_php dal/db_session.php
+++ mysql_php dal/db_session.php
@@ -4,9 +4,9 @@ class db_session {
 	private $connection;
 	
 	public function __construct() {    
-    	$dsn = 'mysql:dbname=worki723_shecodes;host=localhost';
-		$user = 'worki723_shecode';
-		$password = 'shecodes12345';
+    	$dsn = '';
+		$user = '';
+		$password = '';
 			
 		try {
 			$this->connection = new PDO($dsn, $user, $password);
